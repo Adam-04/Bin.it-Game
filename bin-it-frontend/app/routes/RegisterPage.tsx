03c46144158b811e3f93fe2eaf9c896842aa8877
pagdamag <patricia.agdamag@ucalgary.ca>
2026-03-14 17:19:57 -0600

    Updated: Completed registration and login flow for frontend and database communication

--- bin-it-frontend/app/routes/RegisterPage.tsx
+++ bin-it-frontend/app/routes/RegisterPage.tsx
@@ -1,7 +1,56 @@
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
+import { useState } from "react";
+import { authApi } from "../api/auth";
 import "./css/register.css";
 
 export default function RegisterPage() {
+  // Creating States and navigate
+  const [username, setUsername] = useState("");
+
+  const [email, setEmail] = useState("");
+  const [confirmEmail, setConfirmEmail] = useState("");
+
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleRegister = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setLoading(true);
+    setError(""); 
+
+    let errorMessages = [];
+
+    if (email !== confirmEmail) {
+      errorMessages.push("Email Address and Confirm Email Address must match.");
+    }
+
+    if (password !== confirmPassword) {
+      errorMessages.push("Password and Confirm Password must match.");
+    }
+
+    if (errorMessages.length > 0) {
+      setError(errorMessages.join(" "));
+      setLoading(false);
+      return;
+    }
+
+    setLoading(true);
+
+    try {
+      await authApi.register(username, email, password);
+      navigate("/login");
+    } catch (err: any) {
+      setError("Registration failed. Email or username might be already taken.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <div className="register-page">
       {/* BinIt Logo */}
@@ -15,33 +64,42 @@ export default function RegisterPage() {
 
       {/* Box containing all input fields */}
       <main className="register-containter">
-        <h1 className="register-title">CREATE ACCOUNT</h1>
+        <h1 className="register-title">CREATE AN ACCOUNT</h1>
         
-        <form onSubmit={(e) => e.preventDefault()}>
+        <form onSubmit={handleRegister}>
           <div className="input-field-wrapper">
             <label className="input-field-label">Username</label>
-            <input className="input-field-box" type="text" placeholder="Choose a username" />
+            <input className="input-field-box" type="text" placeholder="Choose a username" 
+              value={username} onChange={(e) => setUsername(e.target.value)} required /> 
           </div>
 
           <div className="input-field-wrapper">
             <label className="input-field-label">Email Address</label>
-            <input className="input-field-box" type="email" placeholder="email@example.com" />
+            <input className="input-field-box" type="email" placeholder="email@example.com"
+              value={email} onChange={(e) => setEmail(e.target.value)} required />
           </div>
 
           <div className="input-field-wrapper">
             <label className="input-field-label">Confirm Email Address</label>
-            <input className="input-field-box" type="email" placeholder="email@example.com" />
+            <input className="input-field-box" type="email" placeholder="email@example.com"
+              value={confirmEmail} onChange={(e) => setConfirmEmail(e.target.value)} required />
           </div>
 
           <div className="input-field-wrapper">
             <label className="input-field-label">Password</label>
-            <input className="input-field-box" type="password" placeholder="••••••••" />
+            <input className="input-field-box" type="password" placeholder="••••••••" 
+            value={password} onChange={(e) => setPassword(e.target.value)} required />
           </div>
 
           <div className="input-field-wrapper">
             <label className="input-field-label">Confirm Password</label>
-            <input className="input-field-box" type="password" placeholder="••••••••" />
+            <input className="input-field-box" type="password" placeholder="••••••••" 
+            value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} required />
           </div>
+
+          {/* Display error message if login fails */}
+          {error && 
+          <p style={{ color: "#8C0000", fontSize: "1rem", marginBottom: "10px" }}>{error}</p>}
 
           <button type="submit" className="register-button">
             JOIN NOW

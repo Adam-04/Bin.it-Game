22731101afab767bf979afe8269a0d39cd390639
Adam-04 <144634921+Adam-04@users.noreply.github.com>
2026-03-26 20:50:28 -0600

    Removed cross-origin requests, updated nginx to act as server side proxy to backend

--- bin-it-frontend/app/api/api.config.ts
+++ bin-it-frontend/app/api/api.config.ts
@@ -1,3 +1,3 @@
 
 // For deployment, we need to use the relative path to the API route
-export const BACKEND_ADDRESS = "https://api.binit.online";
+export const BACKEND_ADDRESS = "https://binit.online/api";
